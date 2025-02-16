bd194a69ece1dcf817a361e4f40598df1beea313
Allan Tsai <wltsai@uci.edu>
2025-02-16 14:43:50 -0800

    photon
    photon tutorial

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -26,7 +26,18 @@ namespace Phantom
         string gameVersion = "1";
 
         #endregion
-
+        
+        #region Public Fields
+        
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+        
+        #endregion
+        
         #region MonoBehaviour CallBacks
 
         /// <summary>
@@ -44,14 +55,16 @@ namespace Phantom
         /// </summary>
         void Start()
         {
-            Connect();
+            //Connect();
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
         }
 
         #endregion
 
 
         #region Public Methods
-
+        
         /// <summary>
         /// Start the connection process.
         /// - If already connected, we attempt joining a random room
@@ -59,6 +72,8 @@ namespace Phantom
         /// </summary>
         public void Connect()
         {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.IsConnected)
             {
@@ -88,6 +103,8 @@ namespace Phantom
 
         public override void OnDisconnected(DisconnectCause cause)
         {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
             Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
         }
 
