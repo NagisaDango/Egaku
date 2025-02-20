1feb1329b236fbfcdb199a75b6507f5c7c7dc17c
Aoli3667 <148655828+Aoli3667@users.noreply.github.com>
2025-02-20 12:32:47 -0800

    drawer panel ui
    drawer ui anim

---
+++ Assets/Scripts/Player/Drawer/PenUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PenUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
