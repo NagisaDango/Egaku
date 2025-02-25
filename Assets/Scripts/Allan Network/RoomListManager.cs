7530397508da82e7db49660386df5e36012a2ba6
zsj <Jack_zhang.sj@outlook.com>
2025-02-25 06:01:46 -0800

    NEED FIX

--- Assets/Scripts/Allan Network/RoomListManager.cs
+++ Assets/Scripts/Allan Network/RoomListManager.cs
@@ -5,6 +5,12 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 using TMPro;
+using WebSocketSharp;
+
+using ExitGames.Client.Photon;
+using UnityEditor;
+using Unity.VisualScripting;
+using ExitGames.Client.Photon.StructWrapping;
 
 public class RoomListManager : MonoBehaviourPunCallbacks
 {
@@ -13,6 +19,29 @@ public class RoomListManager : MonoBehaviourPunCallbacks
 
     public TMP_InputField nameField;
 
+    //struct Room {  public string[] playerNames; }
+
+    public Dictionary<string,  string> rooms = new Dictionary<string, string>();
+
+
+    public static RoomListManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            PhotonNetwork.AddCallbackTarget(this); // Listen for events
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this); // Remove listener
+    }
 
     void Start()
     {
@@ -33,6 +62,11 @@ public class RoomListManager : MonoBehaviourPunCallbacks
 
     public void CreateJoinButton()
     {
+        Debug.Log("Enter CreateJoinButton");
+        if (nameField.text.IsNullOrEmpty())
+        {
+            return;
+        }
 
 
         if (PhotonNetwork.IsConnectedAndReady && PhotonNetwork.InLobby)
@@ -46,17 +80,124 @@ public class RoomListManager : MonoBehaviourPunCallbacks
     public void JoinButton(string name )
     {
         PhotonNetwork.JoinRoom(name);
+    }
+
+
+    string test = "";
+    [PunRPC]
+    void UpdateRoomPlayerList()
+    {
+        // Get current room properties
+        //ExitGames.Client.Photon.Hashtable roomProperties = PhotonNetwork.CurrentRoom.CustomProperties;
+
+        // Get existing player list or create a new one
+        //string playerList = roomProperties.ContainsKey("PlayerList") ? (string)roomProperties["PlayerList"] : "";
+
+        string playerList;
+        Debug.Log($"RoomName: {PhotonNetwork.CurrentRoom.Name}");
+        // Add the new player name (avoid duplicates)
+        //if (!rooms.ContainsKey(PhotonNetwork.CurrentRoom.Name))
+        //{
+        //    rooms.Add(PhotonNetwork.CurrentRoom.Name, (nameField.text, ""));
+        //}
+        //else
+        //{
+        //    var players = rooms[PhotonNetwork.CurrentRoom.Name];
+        //    if (players.Item1 != "")
+        //    {
+        //        players = (nameField.text, players.Item2);
+        //    }
+        //    else
+        //    {
+        //        players = (players.Item1, nameField.text);
+        //    }
+        //    rooms[PhotonNetwork.CurrentRoom.Name] = players;
+        //}
+
+        if(!rooms.ContainsKey(PhotonNetwork.CurrentRoom.Name))
+        {
+            rooms.Add(PhotonNetwork.CurrentRoom.Name, nameField.text+ ",");
+        }
+        else
+        {
+            var players = rooms[PhotonNetwork.CurrentRoom.Name].Split(",");
+            string rnames;
+            if (players[0] == "")
+            {
+                rnames = (nameField.text + "," + players[1]);
+            }
+            else
+            {
+                rnames = players[0] + "," + nameField.text;
+            }
+            rooms[PhotonNetwork.CurrentRoom.Name] = rnames;
+        }
+
+
+        test = PhotonNetwork.CurrentRoom.Name;
+        SendPlayerData(new Dictionary<string, string> { });
+        //// Save back to room properties
+        //roomProperties["PlayerList"] = playerList;
+        //Debug.Log($"Update Players: {playerList}");
+        //PhotonNetwork.CurrentRoom.SetCustomProperties(roomProperties);
+
+        //string players = (string)PhotonNetwork.CurrentRoom.CustomProperties["PlayerList"];
+        //Debug.Log($"Update Players: {players}");
+        //print(PhotonNetwork.CurrentRoom.Name);
+        //print(rooms);
+        //rooms.Add(PhotonNetwork.CurrentRoom.Name, playerList);
+
+        //string playerList = roomProperties.ContainsKey("PlayerList") ? (string)roomProperties["PlayerList"] : "";
+
 
     }
+
+    // 发送变量到所有玩家
+    public void SendPlayerData(Dictionary<string, string>  playerNames)
+    {
+        object content = playerNames; // 你可以传递多个值
+        RaiseEventOptions options = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+        SendOptions sendOptions = new SendOptions { Reliability = true };
+
+        PhotonNetwork.RaiseEvent(1, content, options, sendOptions); // 事件代码 "1"
+    }
+
+    // 监听事件
+    public void OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == 1) // 事件代码 "1"
+        {
+            object data = (object)photonEvent.CustomData;
+            Dictionary<string, string> roomNames = (Dictionary<string, string>)data;
+
+            //Debug.Log($"Room: {roomNames}");
+            //rooms = roomNames;
+        }
+    }
+
 
 
     #region Photon Callbacks
 
+    public override void OnCreatedRoom()
+    {
+
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        //UpdateRoomPlayerList(); 
+        //PhotonView photonView = PhotonView.Get(this);
+        //photonView.RPC("UpdateRoomPlayerList", RpcTarget.All);
+        
+    }
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Room successfully created! Now joining the room...");
 
+        UpdateRoomPlayerList();
+        //photonView.RPC("UpdateRoomPlayerList", RpcTarget.All);
+        print("Room X" +  PhotonNetwork.CurrentRoom.Name);
         //PhotonNetwork.JoinRoom(nameField.text);
         //PhotonNetwork.JoinRandomRoom();
         Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
@@ -65,6 +206,7 @@ public class RoomListManager : MonoBehaviourPunCallbacks
         {
             Debug.Log("We load the RoleSelection");
 
+            //UpdateRoomPlayerList();
             // #Critical
             // Load the Room Level.
             PhotonNetwork.LoadLevel("RoleSelection");
@@ -81,6 +223,7 @@ public class RoomListManager : MonoBehaviourPunCallbacks
 
                 if (roomList[i].PlayerCount == 0)
                 {
+                    rooms.Remove(roomList[i].Name);
                     roomList.Remove(roomList[i]);
                 }
 
@@ -91,11 +234,28 @@ public class RoomListManager : MonoBehaviourPunCallbacks
         foreach (RoomInfo room in roomList)
         {
             //GameObject newRoom = Instantiate(roomItemPrefab, gridLayout.position, Quaternion.identity);
-            GameObject newRoom = Instantiate(roomItemPrefab, gridLayout.transform);
-            
-            newRoom.GetComponentInChildren<TMP_Text>().text = room.Name;// + "(" + room.PlayerCount +")";
-            newRoom.GetComponentInChildren<Button>().onClick.AddListener(() => JoinButton(room.Name));
+            Transform newRoom = Instantiate(roomItemPrefab, gridLayout.transform).transform;
             //newRoom.transform.SetParent(gridLayout);
+
+            string playerList;
+            Debug.Log($"Rooms: {rooms.Keys},{rooms.ContainsKey(room.Name)},{test}, {room.Name}");
+            if (rooms.ContainsKey(room.Name))
+            {
+                //playerList = (string)room.CustomProperties["PlayerList"];
+                var players = rooms[room.Name];
+                //newRoom.transform.Find("PlayerName").GetComponent<TMP_Text>().text = players.Item1 = " x " + players.Item2;
+                newRoom.transform.Find("PlayerName").GetComponent<TMP_Text>().text = players;
+                Debug.Log($"Room: {room.Name}, Players: {players}");
+            }
+            else
+            {
+                Debug.Log($"Room: {room.Name}, PlayerList not found in CustomProperties.");
+            }
+
+
+            newRoom.transform.Find("RoomName").GetComponent<TMP_Text>().text = room.Name;// + "(" + room.PlayerCount +")";
+            newRoom.GetComponentInChildren<Button>().onClick.AddListener(() => JoinButton(room.Name));
+
         }
     }
 
