d2906a7983738279d9843778e095a4bc7c49bd49
Aoli3667 <148655828+Aoli3667@users.noreply.github.com>
2025-05-23 11:26:23 -0700

    Glass break effecr

--- Assets/Scripts/Level/Level Props/Breakable Platform.cs
+++ Assets/Scripts/Level/Level Props/Breakable Platform.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Photon.Pun;
 using UnityEngine;
 
@@ -6,11 +7,102 @@ public class BreakablePlatform : MonoBehaviourPun
 {
     [SerializeField] private float breakThreshhold = 40f;
 
+    public IEnumerator SplitMesh(bool destroy)
+    {
+
+        if (GetComponent<MeshFilter>() == null || GetComponent<SkinnedMeshRenderer>() == null)
+        {
+            yield return null;
+        }
+
+        if (GetComponent<Collider>())
+        {
+            GetComponent<Collider>().enabled = false;
+        }
+
+        Mesh M = new Mesh();
+        if (GetComponent<MeshFilter>())
+        {
+            M = GetComponent<MeshFilter>().mesh;
+        }
+        else if (GetComponent<SkinnedMeshRenderer>())
+        {
+            M = GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        }
+
+        Material[] materials = new Material[0];
+        if (GetComponent<MeshRenderer>())
+        {
+            materials = GetComponent<MeshRenderer>().materials;
+        }
+        else if (GetComponent<SkinnedMeshRenderer>())
+        {
+            materials = GetComponent<SkinnedMeshRenderer>().materials;
+        }
+
+        Vector3[] verts = M.vertices;
+        Vector3[] normals = M.normals;
+        Vector2[] uvs = M.uv;
+        for (int submesh = 0; submesh < M.subMeshCount; submesh++)
+        {
+
+            int[] indices = M.GetTriangles(submesh);
+
+            for (int i = 0; i < indices.Length; i += 3)
+            {
+                Vector3[] newVerts = new Vector3[3];
+                Vector3[] newNormals = new Vector3[3];
+                Vector2[] newUvs = new Vector2[3];
+                for (int n = 0; n < 3; n++)
+                {
+                    int index = indices[i + n];
+                    newVerts[n] = verts[index];
+                    newUvs[n] = uvs[index];
+                    newNormals[n] = normals[index];
+                }
+
+                Mesh mesh = new Mesh();
+                mesh.vertices = newVerts;
+                mesh.normals = newNormals;
+                mesh.uv = newUvs;
+
+                mesh.triangles = new int[] { 0, 1, 2, 2, 1, 0 };
+
+                GameObject GO = new GameObject("Triangle " + (i / 3));
+                //GO.layer = LayerMask.NameToLayer("Particle");
+                GO.transform.position = transform.position;
+                GO.transform.rotation = transform.rotation;
+                GO.AddComponent<MeshRenderer>().material = materials[submesh];
+                GO.AddComponent<MeshFilter>().mesh = mesh;
+                GO.AddComponent<BoxCollider>();
+                Vector3 explosionPos = new Vector3(transform.position.x + UnityEngine.Random.Range(-0.5f, 0.5f), transform.position.y + UnityEngine.Random.Range(0f, 0.5f), transform.position.z + UnityEngine.Random.Range(-0.5f, 0.5f));
+                GO.AddComponent<Rigidbody>().AddExplosionForce(UnityEngine.Random.Range(300, 500), explosionPos, 5);
+                Destroy(GO, 5 + UnityEngine.Random.Range(0.0f, 5.0f));
+            }
+        }
+
+        GetComponent<Renderer>().enabled = false;
+
+        //yield return new WaitForSeconds(1.0f);
+        if (destroy == true)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    [PunRPC]
+    public void RPC_BreakEffect()
+    {
+        StartCoroutine(SplitMesh(true));
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Draw") && other.rigidbody.mass >= breakThreshhold)
         {
-            PhotonNetwork.Destroy(this.gameObject);
+            photonView.RPC("RPC_BreakEffect", RpcTarget.All);
+            //PhotonNetwork.Destroy(this.gameObject);
         }
     }
 }
